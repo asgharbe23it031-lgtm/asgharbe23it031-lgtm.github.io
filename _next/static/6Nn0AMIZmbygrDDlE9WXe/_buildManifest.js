14472ea930988cdc154fbdc95c1468e2451c3d8d
Adesh Chaudhary <adeshpatel700@gmail.com>
2026-03-03 12:48:08 +0530

    Updates

---
+++ _next/static/6Nn0AMIZmbygrDDlE9WXe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/635-e52d9531c8ebbc84.js","static/chunks/pages/index-02157ab3e974afd1.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
